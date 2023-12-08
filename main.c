c0c000f6ac50215efb8921873cc4edfb40277ded
Chopper <43015696+Chopper1337@users.noreply.github.com>
2023-12-08 09:17:42 +0000

    Use double For large results, the int was too small.

--- main.c
+++ main.c
@@ -2,43 +2,40 @@
 #include <stdlib.h> //atoi
 
 // Basic factorial
-int factorial(int n){
-  int factorial = n;
-  // Multiply n by n - i where i is all numbers less than n and greater than 0
-  for(int i = n-1;i > 0;i--){
-    factorial *= (n - i);
-  }
-  return factorial;
+double factorial(double n) {
+    double factorial = n;
+    // Multiply n by n - i, where i is all numbers less than n and greater than 0
+    for (double i = n - 1; i > 0; i--)
+        factorial *= (n - i);
+
+    return factorial;
 }
 
 // Superfactorial
-int superfactorial(int n){
-  int f = factorial(n);
-  // Multiply n! by (n-i)! where i is all numbers less than n and greater than 0
-  for(int i = n-1; i > 0; i--){
-    f *= factorial(i);
-  }
+double superfactorial(double n) {
+    double f = factorial(n);
+    // Multiply n! by (n-i)! where i is all numbers less than n and greater than 0
+    for (double i = n - 1; i > 0; i--)
+        f *= factorial(i);
 
-  return f;
+    return f;
 }
 
-int main(int argc, char *argv[])
-{
-  // If the number of args is less than 2, the user hasn't provided a number
-  if(argc < 2){
-    int a = -1;
-    // Prompt the user for a number, hope they actually input a number
-    printf("Enter a positive integer: ");
-    scanf("%d", &a);
+int main(int argc, char *argv[]) {
+    // If the number of args is less than 2, the user hasn't provided a number
+    if (argc < 2) {
+        int a = -1;
+        // Prompt the user for a number, hope they actually input a number
+        printf("Enter a positive integer: ");
+        scanf("%d", &a);
 
-    // Show the result clearly
-    printf("The superfactorial of %d is %d\n", a, superfactorial(a));
-  }
-  // If the user has provided an arg, directly show the result
-  else {
-    printf("%d\n", superfactorial(atoi(argv[1])));
-  }
+        // Show the result clearly
+        printf("The superfactorial of %d is %.lf\n", a, superfactorial(a));
+    }
+        // If the user has provided an arg, directly show the result
+    else
+        printf("%lf.\n", superfactorial(atoi(argv[1])));
 
-  return 0;
+    return 0;
 }
 
